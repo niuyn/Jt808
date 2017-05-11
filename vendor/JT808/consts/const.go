f9bd2e05461f530d306d5795c6f23838f0608c8b
nyn <nyn018@zjqunshuo.com>
2017-05-11 18:04:03 +0800

    first commit

---
+++ vendor/JT808/consts/const.go
@@ -0,0 +1,9 @@
+package Jtconsts
+
+const (
+	DEV_REGISTER uint = 0x0100 //终端注册
+	DEV_LOGOFF   uint = 0x0003 //终端注销
+	DEV_AUTH     uint = 0x0102 //终端鉴权
+	DEV_GPS      uint = 0x0200 //位置信息汇报
+	DEV_BATCHGPS uint = 0x0704 //批量数据上传
+)
---
+++ vendor/JT808/gt808utils.go
@@ -0,0 +1,29 @@
+package JT808
+
+import (
+	"hex"
+)
+
+func generateMsgBodyProps() []byte {
+	return nil
+}
+
+func generateMsgHeader() []byte {
+	return nil
+}
+
+func generateCheckSum() byte {
+	return nil
+}
+
+func currSequence() int {
+	return 0
+}
+
+func doEscape4Receive(buff []byte) (error, []byte) {
+	return nil, nil
+}
+
+func doEscape4Send(buff []byte) (error, []byte) {
+	return nil, nil
+}
